ab14c15d4a16e4ed0ab99fa1f46690fe34977023
juhwanHeo <juhwanheo.dev@gmail.com>
2024-06-01 19:33:20 +0900

    feat: add application - board

---
+++ board/src/main/java/com/study/board/controller/dto/BoardResponseDto.java
@@ -0,0 +1,15 @@
+package com.study.board.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardResponseDto {
+    private String title;
+    private String content;
+}
