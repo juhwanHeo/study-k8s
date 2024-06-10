50b1ae127d96bfeda377edf3d720cd97e230f250
juhwanHeo <juhwanheo.dev@gmail.com>
2024-06-10 12:06:23 +0900

    feat: add application name property

--- board/src/main/java/com/study/board/controller/dto/BoardResponseDto.java
+++ board/src/main/java/com/study/board/controller/dto/BoardResponseDto.java
@@ -10,6 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardResponseDto {
+    private String applicationName;
     private String title;
     private String content;
 }
