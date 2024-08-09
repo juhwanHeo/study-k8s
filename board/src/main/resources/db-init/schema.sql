a836e83c149d238bb9226ee5cb1da8ff0e8e16bb
juhwanHeo <juhwanheo.dev@gmail.com>
2024-08-10 00:40:45 +0900

    fix: Update database(h2)

---
+++ board/src/main/resources/db-init/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE tb_board (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    content TEXT,
+    creator VARCHAR(255)
+);
---
+++ board/src/main/resources/db-init/test-data.sql
@@ -0,0 +1,11 @@
+-- init.sql
+INSERT INTO tb_board (title, content, creator) VALUES ('title 1', 'blabla 01', 'creator 1');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 2', 'blabla 02', 'creator 2');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 3', 'blabla 03', 'creator 3');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 4', 'blabla 04', 'creator 4');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 5', 'blabla 05', 'creator 5');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 6', 'blabla 06', 'creator 6');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 7', 'blabla 07', 'creator 7');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 8', 'blabla 08', 'creator 8');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 9', 'blabla 09', 'creator 9');
+INSERT INTO tb_board (title, content, creator) VALUES ('title 10', 'blabla 10', 'creator 10');
