ab8016c2c6658911984269858bb46045baac84be
juhwanHeo <juhwanheo.dev@gmail.com>
2024-08-19 16:13:21 +0900

    feat: add ignore cross-origin to board

--- board/src/main/java/com/study/board/controller/BoardController.java
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/board")
 @RequiredArgsConstructor
 public class BoardController {
