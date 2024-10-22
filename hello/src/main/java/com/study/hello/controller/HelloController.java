776371d7e7c06828ffcc522689bd557681c935be
Juwon Lee <juwonlee.dev@gmail.com>
2024-10-22 16:02:04 +0900

    feat: add hello endpoint(be the same as BoardApplication)

---
+++ hello/src/main/java/com/study/hello/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.study.hello.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info(">>> hello");
+        return "Hello World";
+    }
+}
